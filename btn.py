7c2a00d448bc9d20f1f3d6f9c9fb0b8fbfba77ef
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-07-29 20:12:55 +0100

    Add files via upload

--- btn.py
+++ btn.py
@@ -1,23 +1,31 @@
 import tkinter as tk
 from tkinter import ttk
 import copy
+import os
 
 class mes:
-    def __init__(self,root):
+    def __init__(self,root,runers:list):
+        self.runers=runers
         self.root=root
         root.title("interface")
         root.geometry("640x480")
         self.labels=[]
         self.lab=[]
         for f in range(12):
-            l=copy.copy("f"+str(copy.copy(f)))
-            self.lab.append(l)
-            self.labels.append(tk.Button(root,text="f"+str(f),command=lambda idx=f: self.f(idx)).pack(ipadx=10,ipady=10))
+            ff="empty"
+            if f<len(self.runers):
+                ff=self.runers[f]
+            else:
+                ff="f"+str(f)
+            self.labels.append(tk.Button(root,text=ff,command=lambda idx=f: self.f(idx)).pack(ipadx=10,ipady=10))
        
     def f(self,value:str):
-        print(value)
+        if value>=len(self.runers):
+            print("error")
+        else:
+            os.system(self.runers[value])
          
-
+runers=["notepad.exe","explorer.exe","pbrush.exe","write.exe","word.exe","exel.exe","edge.exe"]
 root = tk.Tk()
-mesme=mes(root)
+mesme=mes(root,runers)
 root.mainloop()
